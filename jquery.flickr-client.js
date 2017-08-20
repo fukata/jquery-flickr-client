313b1dec469a4cd61707538fb1d4db19704c7d87
fukata <tatsuya.fukata@gmail.com>
2017-08-20 16:39:29 +0700

    change parameter name auth_token to oauth_token

--- jquery.flickr-client.js
+++ jquery.flickr-client.js
@@ -1,5 +1,5 @@
 ;
-/* jquery.flickr-client.js v1.0.0
+/* jquery.flickr-client.js v1.1.0
  * */
 (function($) {
 
@@ -20,8 +20,8 @@
   }
 
   /**
-   * return HashMap by HashMap's key sorted. 
-   * 
+   * return HashMap by HashMap's key sorted.
+   *
    * @return
    */
   function ksort(params) {
@@ -34,16 +34,16 @@
         sorted[key] = params[key];
       }
     });
-    
+
     return sorted;
   };
-  
+
   /**
    * Convert HashMap to query string.
    */
   function hash2query(params) {
     var query = "";
-    
+
     $.each(params, function(key, val){
       var _query = "";
       if ($.isArray(val)) {
@@ -54,14 +54,14 @@
       } else {
         _query = key + "=" + val;
       }
-      
+
       if (query.length>0) query += "&";
       query += _query;
     });
-    
+
     return query;
   };
-  
+
   function FlickrClient(options) {
     this.options = $.extend(FlickrClient.prototype.DEFAULT_OPTIONS, options);
   }
@@ -76,7 +76,7 @@
   FlickrClient.prototype.SIZES = {
     "sq": "url_sq", // Square 75
     "q": "url_q",   // Square 150
-    "t": "url_t",   // Thumbnail 
+    "t": "url_t",   // Thumbnail
     "s": "url_s",   // Small 240
     "n": "url_n",   // Small 320
     "m": "url_m",   // Medium 500
@@ -87,8 +87,8 @@
     "k": "url_k",   // Large 2048
     "o": "url_o"  // Original
   };
-  FlickrClient.prototype.SIZE_KEYS   = hkeys(FlickrClient.prototype.SIZES); 
-  FlickrClient.prototype.SIZE_VALUES = hvalues(FlickrClient.prototype.SIZES); 
+  FlickrClient.prototype.SIZE_KEYS   = hkeys(FlickrClient.prototype.SIZES);
+  FlickrClient.prototype.SIZE_VALUES = hvalues(FlickrClient.prototype.SIZES);
 
   FlickrClient.prototype.SIZE_LABELS = {
     "sq": "Square",
@@ -104,11 +104,11 @@
     "k": "Large",
     "o": "Original"
   };
-  FlickrClient.prototype.SIZE_LABEL_VALUES = hvalues(FlickrClient.prototype.SIZE_LABELS); 
+  FlickrClient.prototype.SIZE_LABEL_VALUES = hvalues(FlickrClient.prototype.SIZE_LABELS);
 
   /**
    * execute request.
-   * 
+   *
    * @param method
    *      Flickr method
    * @param params
@@ -124,21 +124,21 @@
       method: method,
       format: "json",
       api_key: this.options.apiKey,
-      auth_token: this.options.oauthToken,
+      oauth_token: this.options.oauthToken,
       user_id: this.options.userId
     }, params);
-    
+
     var type = this.getHttpMethod(method);
     var url = this.options.restEndpoint;
     var async = params['async'] || true;
     delete params['async'];
-    
+
     var callbackName = 'flickr_callback_' + Math.floor( Math.random() * 100000000 );
     window[callbackName] = function(res) {
       if ( $.isFunction(callback) ) callback(res);
       delete window[callbackName];
     };
-    
+
     params['jsoncallback'] = callbackName;
     // for disable cache
     params['_'] = $.now();
@@ -147,7 +147,7 @@
       params['api_sig'] = this.generateSignature(params);
     }
     params['jsoncallback'] = '?';
-    
+
     var ajaxOption = {
       type: type,
       url: url + '?' + hash2query(params),
@@ -160,13 +160,13 @@
     if ( $.isFunction(errorCallback) ) {
       ajaxOption['error'] = errorCallback;
     }
-    
+
     return $.ajax(ajaxOption);
   };
-  
+
   /**
    * return Http method
-   * 
+   *
    * @param method
    *      Flickr method
    * @return Http method
@@ -190,11 +190,11 @@
         sig += String(key) + String(val);
       }
     });
-    
+
     sig = this.options.apiSecret + sig;
     return $.md5(sig);
   };
-  
+
   FlickrClient.prototype.photos_search = function(options, callback, errorCallback){
       return this.request("flickr.photos.search", options, callback, errorCallback);
   };
@@ -208,11 +208,11 @@
   FlickrClient.prototype.photosets_getPhotos = function(options, callback, errorCallback){
     return this.request("flickr.photosets.getPhotos", options, callback, errorCallback);
   };
-  
+
   FlickrClient.prototype.tags_getListUser = function(options, callback, errorCallback) {
     return this.request("flickr.tags.getListUser", options, callback, errorCallback);
   };
-  
+
   FlickrClient.prototype.getPhotoUrl = function(photo, size) {
     size = size || "m";
 
